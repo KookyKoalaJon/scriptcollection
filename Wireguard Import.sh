6fcdff7dee7931774b23c9ae189dbe94662b105a
Jon <47540419+KookyKoalaJon@users.noreply.github.com>
2026-02-20 02:39:14 +0000

    Add files via upload

--- Wireguard Import.sh
+++ Wireguard Import.sh
@@ -5,7 +5,7 @@ ICON=$HOME/Scripts/wireguard.png
 kdialog --icon "$ICON" --title "Wireguard Import" --msgbox "Wireguard import script, you will be prompted to choose a file<br>please choose your Wireguard config file.<br>Make sure your config is only upercase, lowercase, and - to avoid errors"
 # Check for ok, if not exit progam
   if [ $? = 0 ]; then
-    # Select config file, set VAR1 for input
+# Select config file, set VAR1 for input
     VAR1=$(kdialog --icon "$ICON" --title "Choose Wireguard config" --getopenfilename $HOME/Downloads ;)
       if [ $? = 0 ]; then
 # Import file using Network Manager
@@ -14,7 +14,11 @@ kdialog --icon "$ICON" --title "Wireguard Import" --msgbox "Wireguard import scr
         VAR2=$(basename $VAR1 .conf)
 # Bring down the network connection for wireguard connection
         nmcli connection down $VAR2
-#Completed message, ask if user would like to connect.
+#Completed message, ask if auto connect should be enabled.
+        kdialog --icon "$ICON" --title "Wireguard Import" --yesno "Would you ike to Auto Connect on new interface $VAR2"
+          if [ $? = 1 ]; then
+            nmcli connection modify $VAR2 connection.autoconnect no
+          fi
         kdialog --icon "$ICON" --title "Wireguard Import" --yes-label "Connect" --no-label "Close" --yesno "Wireguard Config has been imported you can connect using the netowrk gui. Would you like to connect now?"
 # Check if Connect was pressed if so bring interface up if not close.
         if [ $? = 0 ]; then
